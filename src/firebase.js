3c2be0d49b1dfb90c410b6a3cb1095d65f0b874d
Roger Campbell II <rc1336@gmail.com>
2019-09-05 22:33:50 -0400

    Finished set up of Firebase DB.

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { firebase } from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseUrl: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: ''
+    apiKey: "AIzaSyAqP75rbQ6v85_QoyPNmFw_6r1NbZQY_h0",
+    authDomain: "todolist-tutorial1336.firebaseapp.com",
+    databaseURL: "https://todolist-tutorial1336.firebaseio.com",
+    projectId: "todolist-tutorial1336",
+    storageBucket: "todolist-tutorial1336.appspot.com",
+    messagingSenderId: "623567305166",
+    appId: "1:623567305166:web:3ae5a43076df9fdbd6139f"
 })
 
 export { firebaseConfig as firebase }
